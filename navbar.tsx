bdab4961e85de06c913bf8fd9853b930ed080502
Supriya-web25 <supriya00025@gmail.com>
2025-07-28 15:25:32 +0530

    Create navbar.tsx

---
+++ navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '1rem 2rem', display: 'flex', justifyContent: 'space-between' }}>
+      <h1>Lumora</h1>
+      <span>Let Stories Glow</span>
+    </nav>
+  )
+}
