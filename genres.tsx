8d771d67118ef1437d5b5371be3ad2b1bf0cbe18
Supriya-web25 <supriya00025@gmail.com>
2025-07-28 15:27:17 +0530

    Create genres.tsx

---
+++ genres.tsx
@@ -0,0 +1,13 @@
+export default function Genres() {
+  return (
+    <section>
+      <h2>Popular Genres</h2>
+      <div>
+        <span>Thriller</span> | 
+        <span>Horror</span> | 
+        <span>Sci-Fi</span> | 
+        <span>Dystopia</span>
+      </div>
+    </section>
+  )
+}
