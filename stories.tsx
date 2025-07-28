3d37652dd6e75c75a4d8a539ce63987ed5fbbc41
Supriya-web25 <supriya00025@gmail.com>
2025-07-28 15:28:42 +0530

    Create stories.tsx

---
+++ stories.tsx
@@ -0,0 +1,12 @@
+export default function Stories() {
+  return (
+    <section>
+      <h2>Latest Series</h2>
+      <div>
+        <h3>The Echo Files</h3>
+        <p>A detective hears voices of the dead...</p>
+        <button>Play Episode</button>
+      </div>
+    </section>
+  )
+}
