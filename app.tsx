53b7b4c93deec9d71e6baddd18e2ad3c664a0c3f
Supriya-web25 <supriya00025@gmail.com>
2025-07-28 15:24:17 +0530

    Create app.tsx

---
+++ app.tsx
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Genres from './components/Genres'
+import Stories from './components/Stories'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Genres />
+      <Stories />
+      <Footer />
+    </>
+  )
+}
